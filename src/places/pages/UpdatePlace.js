19a09fe119a509c03d44b8397af1ca2cc3002df5
Rohit2288 <99073517+Rohit2288@users.noreply.github.com>
2024-07-21 22:02:28 +0530

    Add files via upload

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,275 @@
+// import React , {useEffect , useState , useContext} from 'react'
+// import { useParams , useHistory } from 'react-router-dom'
+
+// import Input from '../../shared/components/FormElements/Input'
+// import Button from '../../shared/components/FormElements/Button'
+// import Card from '../../shared/components/UIElements/card'
+// import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
+// import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+// import { VALIDATOR_REQUIRE , VALIDATOR_MINLENGTH } from '../../shared/util/validator'
+
+// import { useForm } from '../../shared/hooks/form-hook'
+// import { useHttpClient } from '../../shared/hooks/http-hook'
+// import { AuthContext } from '../../shared/context/auth-context';
+// import './PlaceForm.css';
+
+
+// const UpdatePlace = () => {
+//   const auth = useContext(AuthContext);
+//   const {sendRequest , loading , error , clearError} = useHttpClient();
+//   const [loadedPlace, setLoadedPlace] = useState();
+//   const placeId = useParams().placeId;
+//   const history = useHistory();
+
+
+
+//   const [formState , inputHandler , setFormData] = useForm({
+//     title:{
+//         value: '',
+//         isValid: false
+//     },
+//     description:{
+//         value: '',
+//         isValid: false
+//     }
+//   } , false);
+
+//  useEffect(()=>{
+//     const fetchPlace = async() =>{
+//         try {
+//             const responseData = await sendRequest
+//             (`http://localhost:5001/api/places/${placeId}`); 
+//             console.log('Fetched place:', responseData); 
+//             setLoadedPlace(responseData.place);
+//             setFormData({
+//                 title:{
+//                     value: responseData.place.title,
+//                     isValid: true
+//                 },
+//                 description:{
+//                     value: responseData.place.description,
+//                     isValid: true
+//                 }
+//               } , true);
+//         } catch (err) {
+//             console.error('Error fetching place:', err);
+//         }       
+//     };
+//     fetchPlace();
+//  } , [sendRequest , placeId , setFormData]);
+
+
+//   const placeUpdateSubmitHandler = async event =>{
+//     event.preventDefault();
+//     console.log('Submitting form with values:', formState.inputs);
+//     try {
+//         await sendRequest(`http://localhost:5001/api/places/${placeId}` , 
+//             'PATCH' ,
+//             JSON.stringify({
+//                 title: formState.inputs.title.value,
+//                 description: formState.inputs.description.value,
+//             }) , {
+//                 'Content-Type': 'application/json',
+//             }
+//          ); 
+//          history.push('/' + auth.userId + '/places');
+//     } catch (error) {
+//         console.error('Error updating place:', error);
+//     }
+    
+//   }
+
+//   if(loading){
+//     return(
+//         <div className='center'>
+//             <LoadingSpinner />
+//         </div>
+//     )
+//   }
+
+//   if(!loadedPlace  && !error){
+//     return(
+//         <div className='center'>
+//             <Card>
+//             <h2>Could not find place!</h2>
+//             </Card>           
+//         </div>
+//     )
+//   }
+
+  
+
+//   return (
+//     // formState.inputs.title.value && 
+//     <React.Fragment>
+//     <ErrorModal error={error} onClear={clearError}/>
+//     {!loading && loadedPlace && (<form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+//         <Input 
+//         id='title'
+//         element='input'
+//         type='text'
+//         label='Title'
+//         validators={[VALIDATOR_REQUIRE()]}
+//         errorText ='Please enter a valid Title.'
+//         onInput={inputHandler}
+//         initialValue={loadedPlace.title}
+//         initialValid={true}
+//         />
+//         <Input 
+//         id='description'
+//         element='textarea'
+//         label='Description'
+//         validators={[VALIDATOR_MINLENGTH(6)]}
+//         errorText ='Please enter a valid Description(atleast 6 characters).'
+//         onInput={inputHandler}
+//         intialValue={loadedPlace.description}
+//         intialValid={true}
+//         />
+
+//         <Button type='submit' disabled={!formState.isValid} >
+//             UPDATE PLACE
+//         </Button>
+//     </form>)}
+//     </React.Fragment>
+//   )
+// }
+
+// export default UpdatePlace
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/card';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validator';
+
+import { useForm } from '../../shared/hooks/form-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+import './PlaceForm.css';
+
+const UpdatePlace = () => {
+  const auth = useContext(AuthContext);
+  const { sendRequest, loading, error, clearError } = useHttpClient();
+  const [loadedPlace, setLoadedPlace] = useState();
+  const placeId = useParams().placeId;
+  const history = useHistory();
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  useEffect(() => {
+    const fetchPlace = async () => {
+      try {
+        const responseData = await sendRequest(`http://localhost:5001/api/places/${placeId}`);
+        console.log('Fetched place:', responseData);
+        setLoadedPlace(responseData.place);
+        setFormData(
+          {
+            title: {
+              value: responseData.place.title,
+              isValid: true,
+            },
+            description: {
+              value: responseData.place.description,
+              isValid: true,
+            },
+          },
+          true
+        );
+      } catch (err) {
+        console.error('Error fetching place:', err);
+      }
+    };
+    fetchPlace();
+  }, [sendRequest, placeId, setFormData]);
+
+  const placeUpdateSubmitHandler = async (event) => {
+    event.preventDefault();
+    console.log('Submitting form with values:', formState.inputs);
+    try {
+      await sendRequest(
+        `http://localhost:5001/api/places/${placeId}`,
+        'PATCH',
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+        }),
+        {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + auth.token
+        }
+      );
+      history.push('/' + auth.userId + '/places');
+    } catch (error) {
+      console.error('Error updating place:', error);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className='center'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (!loadedPlace && !error) {
+    return (
+      <div className='center'>
+        <Card>
+          <h2>Could not find place!</h2>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {!loading && loadedPlace && (
+        <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+          <Input
+            id='title'
+            element='input'
+            type='text'
+            label='Title'
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText='Please enter a valid Title.'
+            onInput={inputHandler}
+            initialValue={loadedPlace.title}
+            initialValid={true}
+          />
+          <Input
+            id='description'
+            element='textarea'
+            label='Description'
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText='Please enter a valid Description (at least 5 characters).'
+            onInput={inputHandler}
+            initialValue={loadedPlace.description}
+            initialValid={true}
+          />
+          <Button type='submit' disabled={!formState.isValid}>
+            UPDATE PLACE
+          </Button>
+        </form>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default UpdatePlace;
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,45 @@
+import React , {useEffect , useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import PlaceList from '../components/PlaceList'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook'
+
+const UserPlaces = () => {
+    const [loadedPlaces, setLoadedPlaces] = useState();
+    const {loading , error , sendRequest , clearError} = useHttpClient();
+
+    const userId = useParams().userId;
+
+    useEffect( ()=>{
+        const fetchPlaces = async()=>{
+            try {
+                const responseData = await sendRequest(`http://localhost:5001/api/places/user/${userId}`);
+                setLoadedPlaces(responseData.places);
+            } catch (err) {            
+            }          
+        }
+        fetchPlaces();
+    }, [sendRequest , userId]);
+
+    const placeDeletedHandler = (deletedPlaceId) =>{
+        setLoadedPlaces(prevPlaces => 
+            prevPlaces.filter(place => place.id !== deletedPlaceId)
+        ); 
+    }
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError}/>
+      {loading && (
+        <div className='center'>
+            <LoadingSpinner />
+        </div>
+      )}
+      {!loading && loadedPlaces && <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler}/>}
+    </React.Fragment>
+  );
+}
+
+export default UserPlaces
